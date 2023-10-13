11a1756fb74e62f775b1ab50a6a5fa243874b5eb
Ilan Herbach <ilan.herbach@gmail.com>
2023-10-12 22:06:07 -0300

    initial commit

---
+++ src/main/java/com/ilanherbach/javaspringboottodo/JavaSpringbootTodoApplication.java
@@ -0,0 +1,13 @@
+package com.ilanherbach.javaspringboottodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilanherbach/javaspringboottodo/JavaSpringbootTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilanherbach.javaspringboottodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
